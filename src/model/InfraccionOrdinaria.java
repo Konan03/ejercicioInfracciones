ae17e024ca95feeaf779e192e98d2a544bd6291d
Juan Jose Lozano <juan.lozano3@hotmail.com>
2024-02-13 10:01:36 -0500

    create class infraccionOrdinaria

---
+++ src/model/InfraccionOrdinaria.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.time.LocalDate;
+
+public class InfraccionOrdinaria extends Infraccion{
+    private String agente;
+    private String tipoInfracion;
+    private String infractor;
+
+    public InfraccionOrdinaria(int id, LocalDate fecha, double valor, String descripcion, String agente, String tipoInfracion, String infractor) {
+        super(id, fecha, valor, descripcion);
+        this.agente = agente;
+        this.tipoInfracion = tipoInfracion;
+        this.infractor = infractor;
+    }
+
+    public String getAgente() {
+        return agente;
+    }
+
+    public void setAgente(String agente) {
+        this.agente = agente;
+    }
+
+    public String getTipoInfracion() {
+        return tipoInfracion;
+    }
+
+    public void setTipoInfracion(String tipoInfracion) {
+        this.tipoInfracion = tipoInfracion;
+    }
+
+    public String getInfractor() {
+        return infractor;
+    }
+
+    public void setInfractor(String infractor) {
+        this.infractor = infractor;
+    }
+
+    @Override
+    public String toString() {
+        return "InfraccionOrdinaria{" +
+                super.toString() + " " +
+                "agente='" + agente + '\'' +
+                ", tipoInfracion='" + tipoInfracion + '\'' +
+                ", infractor='" + infractor + '\'' +
+                '}';
+    }
+}
